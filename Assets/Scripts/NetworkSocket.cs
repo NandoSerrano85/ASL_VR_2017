6d8d153904dbc99ded4350bfa2edd6ac5a5d0594
Luis Averhoff <laver008@fiu.edu>
2017-09-30 21:21:06 -0400

    Created a network script that will connect to the server.py file. This is the file that unity will send the coordinates of the hands to. The server will then deduce what gesture it is and send it back to unity.

---
+++ Assets/Scripts/NetworkSocket.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using UnityEngine;
+
+public class NetworkSocket : MonoBehaviour
+{
+    public string host = "localhost";
+    public int port = 50000;
+
+    internal bool socketReady = false;
+    internal string inputBuffer = "";
+
+    TcpClient tcpSocket;
+    NetworkStream netStream;
+
+    StreamWriter socketWriter;
+    StreamReader socketReader;
+
+	void Update ()
+    {
+        string receivedData = readSocket(); 
+        string keyStroke = Input.inputString;
+
+        if(keyStroke != "")
+        {
+            inputBuffer += keyStroke;
+
+            if(keyStroke == "\n" || keyStroke == "\r")
+            {
+                Debug.Log("Sending: " + inputBuffer);
+                writeSocket(inputBuffer);
+                inputBuffer = "";
+            }
+        }
+
+        if(receivedData != "")
+        {
+            Debug.Log(receivedData);
+        }
+	}
+
+    void Awake()
+    {
+        setUpSocket();
+    }
+
+    void OnApplicationQuit()
+    {
+        closeSocket();
+    }
+
+    public void setUpSocket()
+    {
+        try
+        {
+            tcpSocket = new TcpClient(host, port);
+
+            netStream = tcpSocket.GetStream();
+            socketReader = new StreamReader(netStream);
+            socketWriter = new StreamWriter(netStream);
+
+            socketReady = true;
+        }
+        catch(Exception e)
+        {
+            Debug.Log("Socket Error: " + e);
+        }
+    } 
+
+    public void writeSocket(string line)
+    {
+        if (!socketReady)
+            return;
+
+        line = line + "\r\n";
+        socketWriter.Write(line);
+        socketWriter.Flush();
+    }
+
+    public string readSocket()
+    {
+        if (!socketReady)
+            return "";
+
+        if (netStream.DataAvailable)
+            return socketReader.ReadLine();
+
+        return "";
+    }
+
+    public void closeSocket()
+    {
+        if (!socketReady)
+            return;
+
+        socketWriter.Close();
+        socketReader.Close();
+        tcpSocket.Close();
+
+        socketReady = false;
+    }
+}
---
+++ Server.py
@@ -0,0 +1,33 @@
+from socket import socket, SOCK_STREAM, AF_INET, SOL_SOCKET, SO_REUSEADDR, SHUT_RDWR
+
+BUFFER_SIZE = 4096
+
+def createServerSocket(protocol_type, socket_type, hostname, port, numberOfConnections):
+    serverSocket = socket(protocol_type, socket_type)
+    serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+    serverSocket.bind((hostname, port))
+    serverSocket.listen(numberOfConnections)
+    return serverSocket
+
+def processClientInput(clientSocket):
+    while True:
+        data = clientSocket.recv(BUFFER_SIZE).decode().rstrip('\r\n')
+
+        if data:
+                clientSocket.send(("You just said: " + data + "\n").encode())
+
+def main():
+    host = ''
+    port = 50000
+    numOfConnections = 5
+
+    serverSocket = createServerSocket(AF_INET, SOCK_STREAM, host, port, numOfConnections)
+
+    while True:
+        clientSocket, address = serverSocket.accept()
+        print("Client connected")
+        clientSocket.send("Hello!\n".encode())
+        processClientInput(clientSocket)
+
+if __name__ == '__main__':
+    main()
