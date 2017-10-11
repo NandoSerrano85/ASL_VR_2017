cfc812b17b58bc1f7b3dd646ff264b37d9e767d2
Luis Averhoff <laver008@fiu.edu>
2017-10-11 01:17:09 -0400

    Refactored all the functions involving loading and adding the recording files to the dropdown box as well the dropdown's selected index change method into its own class.

---
+++ Assets/Scripts/RecordingList.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RecordingList : MonoBehaviour
+{
+    [SerializeField]
+    private Dropdown recordingFilesDropDown;
+
+    [SerializeField]
+    private HandController handController;
+
+    private RecordingControls recordingControls;
+
+    private List<string> recordingList;
+
+    private void Start()
+    {
+        recordingControls = handController.GetComponent<RecordingControls>();
+        recordingList = new List<string>();
+        populateRecordingDropDownList();
+    }
+
+    private void Update()
+    {
+        if (recordingControls.SavedPath != "")
+        {
+            addRecordingToList(Path.GetFileName(recordingControls.SavedPath));
+            recordingControls.SavedPath = "";
+        }
+    }
+
+    private void addRecordingToList(string recordingFile)
+    {
+        recordingList.Add(recordingFile);
+        recordingFilesDropDown.ClearOptions();
+        recordingFilesDropDown.AddOptions(recordingList);
+        recordingFilesDropDown.RefreshShownValue();
+    }
+
+    public void recordingListDropDown_IndexChanged(int index)
+    {
+        string file = "Assets/Resources/Recordings/" + recordingList[index];
+        handController.recordingAsset = (TextAsset)AssetDatabase.LoadAssetAtPath(file, typeof(TextAsset));
+        AssetDatabase.ImportAsset(file);
+        handController.enableRecordPlayback = true;
+    }
+
+    private void populateRecordingDropDownList()
+    {
+        var filePaths = Directory.GetFiles(Application.dataPath + "/Resources/Recordings").Where(name => !name.EndsWith(".meta"));
+
+        foreach (string file in filePaths)
+        {
+            recordingList.Add(Path.GetFileName(file));
+        }
+
+        recordingFilesDropDown.AddOptions(recordingList);
+    }
+}
