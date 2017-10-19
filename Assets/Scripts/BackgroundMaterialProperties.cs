87335f1041a9f5a3a2d6acee5cdd4157a1d53ca4
Luis Averhoff <laver008@fiu.edu>
2017-10-19 01:42:33 -0400

    Whenever the user playbacks a recording file, it blacks out the scene and displays the robotic hands.

---
+++ Assets/Scripts/BackgroundMaterialProperties.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class BackgroundMaterialProperties : MonoBehaviour
+{
+    [SerializeField]
+    private HandController handController;
+
+    private Material backgroundMaterial; 
+
+	// Use this for initialization
+	void Start ()
+    {
+        backgroundMaterial = GetComponent<Renderer>().material;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (handController.GetLeapRecorder().state == RecorderState.Playing)
+        {
+            backgroundMaterial.SetFloat("_ColorSpaceGamma", 0.0f);
+            backgroundMaterial.renderQueue = 1000;
+        }
+        else
+        {
+            backgroundMaterial.SetFloat("_ColorSpaceGamma", 1.9f);
+            backgroundMaterial.renderQueue = 3000;
+        }
+    }
+}
